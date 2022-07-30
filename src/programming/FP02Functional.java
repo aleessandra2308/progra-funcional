1efeb84f167505fb49ae7c218b0dbbfed82e5805
Alessandra Rosado Villalobos <ale.rosado@MacBook-Pro-de-Alessandra.local>
2022-07-30 00:51:58 -0500

    ex reduce 1

---
+++ src/programming/FP02Functional.java
@@ -0,0 +1,20 @@
+package programming;
+
+import java.util.List;
+
+public class FP02Functional {
+    
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(12,9,13,4,6,2,4,12,15);
+        int sum = addListFunctional(numbers);
+        System.out.println(sum);
+    }
+
+    private static int sum(int a, int b){
+        return a+b;
+    }
+
+    public static int addListFunctional(List<Integer> numbers) {
+        return numbers.stream().reduce(0, FP02Functional::sum);
+    }
+}
